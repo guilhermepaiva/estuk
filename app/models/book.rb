870b085b3ecfc2a75833c34749fa90c4b56a495f
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-06-27 15:23:12 -0300

    added sales model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ActiveRecord::Base
 	belongs_to :user
+	has_many :sales
 	has_attached_file :image
 	has_attached_file :resource
 
@@ -8,4 +9,6 @@ class Book < ActiveRecord::Base
 
 	validates :image, attachment_presence: true
 	validates :resource, attachment_presence: true
+	validates_numericality_of :price, greater_than: 49, message: "Must be at least 50 cents"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622144817) do
+ActiveRecord::Schema.define(version: 20150627175050) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20150622144817) do
     t.string   "resource_content_type"
     t.integer  "resource_file_size"
     t.datetime "resource_updated_at"
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.string   "buyer_email"
+    t.string   "seller_email"
+    t.integer  "amount"
+    t.string   "guid"
+    t.integer  "book_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
